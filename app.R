b1f2c3e23b4bddb82c4995c97f7d4fa119436750
kibiras <kdaugela@gmail.com>
2018-11-12 01:45:50 +0300

    a:

--- app.R
+++ app.R
@@ -28,12 +28,12 @@ server <- function(input, output, session) {
     filter(name %in% c("combinationEnd", "combinationStart")) %>%
     select(game_id, combination_id, status, time, name) %>%
     group_by(game_id, combination_id, name) %>%
-    summarize(tries = n(), time = max(time, na.rm = TRUE), status) %>%
-    summarise(start_time = min(time, na.rm = TRUE), end_time = max(time, na.rm = TRUE), tries) %>%
+    summarize(tries = n(), time = max(time, na.rm = TRUE), status = sum(status)) %>%
+    summarise(start_time = min(time, na.rm = TRUE), end_time = max(time, na.rm = TRUE), tries, status) %>%
     mutate(duration = (end_time - start_time)) %>%
-    group_by(game_id)   %>%
-    mutate(points = ifelse(tries == 1, 2, ifelse(tries == 2, 1, 0)))   %>%
-    summarise(time_in_minutes = sum(duration, na.rm = TRUE)/60, score = sum(points, na.rm = TRUE)) %>%
+    group_by(game_id) %>%
+    mutate(points = ifelse(tries == 1 & status == 1, 2, ifelse(tries == 2 & status == 1, 1, 0)))   %>%
+    summarise(time_in_minutes = sum(duration, na.rm = TRUE)/60, completed_shots = sum(status), score = sum(points, na.rm = TRUE)) %>%
     mutate(time_in_minutes = round(time_in_minutes, 2)) %>%
     arrange(desc(score), time_in_minutes) %>%
     collect() %>%
