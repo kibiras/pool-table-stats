b97b0f9e8d555fbafcbc8402cd21062b21c0f0b6
kibiras <kdaugela@gmail.com>
2018-11-12 13:03:13 +0200

    fix

--- app.R
+++ app.R
@@ -14,7 +14,7 @@ ui <- fluidPage(theme = shinytheme("cyborg"),
 )
 
 server <- function(input, output, session) {
-  table_name <- 'event3'
+  table_name <- 'event'
   conn <- dbConnect(
     drv = RMariaDB::MariaDB(), 
     username = 'seb',
